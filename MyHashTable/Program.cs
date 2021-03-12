c79a946d3e0a0dffbdd64f515813769fa856293f
Sergey <shesergey2001@gmail.com>
2021-03-12 11:30:51 +1000

    Pro HashTable

--- MyHashTable/Program.cs
+++ MyHashTable/Program.cs
@@ -6,12 +6,14 @@ namespace MyHashTable
     {
         static void Main(string[] args)
         {
-            var HashTable = new HashTable2<int,string>(100);
-            HashTable.Add(5,"привет");
-            HashTable.Add(18,"мир");
-            HashTable.Add(777,"здравствуй");
-            Console.WriteLine(HashTable.Search(6,"Вася"));
-            Console.WriteLine(HashTable.Search(18,"мир"));
+            var HashTable = new SuperHashTable<int>(20);
+            HashTable.Add(5);
+            HashTable.Add(5);
+            HashTable.Add(5);
+            HashTable.Add(5);
+            HashTable.Add(5);
+
+
 
 
         }
