db48beedcd7f53f47666d830fb2fb9461be307e8
Sergey <shesergey2001@gmail.com>
2021-03-16 10:41:22 +1000

    commented

--- MyHashTable/Item2.cs
+++ MyHashTable/Item2.cs
@@ -8,9 +8,13 @@ namespace MyHashTable
 {
     class Item2
     {
-        public int Key { get; set; }
-        public List Nodes { get; set; }
+        public int Key { get; set; }//ключ
+        public List Nodes { get; set; }//список
 
+        //
+        //Формальные параметры:
+        //Входные данные:
+        //Выходные данные:
         public Item2(int key)
         {
             Key = key;
